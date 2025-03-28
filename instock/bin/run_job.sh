354e334c20a3b976233a0621623866f444b45286
linzhiji <linzhiji@ishehui.com>
2025-03-28 10:33:54 +0800

    fix bug

--- instock/bin/run_job.sh
+++ instock/bin/run_job.sh
@@ -2,7 +2,34 @@
 
 #export PYTHONPATH=/data/InStock
 
-/usr/local/bin/python3 /data/InStock/instock/job/execute_daily_job.py
+# /usr/local/bin/python3 /data/InStock/instock/job/execute_daily_job.py
+
+# 获取脚本所在目录
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# 获取项目根目录（脚本目录的上一级目录）
+base_dir="$( cd "$script_dir/../.." && pwd )"
+echo "base_dir: $base_dir"
+cd $base_dir
+
+
+jobDir="${base_dir}/job"
+echo "jobDir: $jobDir"
+pythonPath=${base_dir}/venv/bin/python
+
+logName=execute_daily_job
+jobName=execute_daily_job.py
+
+. $base_dir/instock/bin/colors.sh
+
+TAILPID=`ps aux | grep "$jobName" | grep -v grep | awk '{print $2}'`
+echo "${YELLOW}check $jobName pid $TAILPID ${NOCOLOR}"
+[ "0$TAILPID" != "0" ] && kill -9 $TAILPID
+
+mkdir -p logs
+
+echo "${YELLOW}nohup $pythonPath $jobDir/$jobName > logs/${logName}.log 2>&1 &${NOCOLOR}"
+nohup $pythonPath $jobDir/$jobName > logs/${logName}.log 2>&1 &
+
 
 echo ------整体作业 支持批量作业------
 echo 当前时间作业 python execute_daily_job.py
